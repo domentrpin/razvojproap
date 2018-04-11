a10fd3c2237a22b517a648948e51358dd3db66cf
B23 <B23@c.scng.si>
2018-04-11 11:23:45 +0200

    Prvi commmit

---
+++ Matrika/Matrika/Matrika.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrika
+{
+    class Matrika
+    {
+        private double[,] matrix = new double[3, 3];
+
+        //indekser
+        public double this[int x, int y]
+        {
+            get
+            {
+                return matrix[x, y];
+            }
+            set
+            {
+                matrix[x, y]=value;
+            }
+        }
+
+        public static int operator +(Matrika a, Matrika b)
+        {
+
+        }
+
+        public void Izpis()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+            }
+            
+        }
+    }
+}
---
+++ Matrika/Matrika/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrika a = new Matrika();
+            Matrika b = new Matrika();
+            Init(a);
+            Init(b);
+
+            Matrika c = a + b;
+            a.Izpis();
+            Console.WriteLine("+");
+
+            Matrika c = a + b;
+            a.Izpis();
+            Console.WriteLine("=");
+
+            Matrika c = a + b;
+            a.Izpis();
+            Console.ReadLine();
+
+        }
+        static void Init(Matrika x)
+        {
+            Random r = new Random();
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    x[i, j] = r.Next(10);
+                }
+            }
+        }
+    }
+}
